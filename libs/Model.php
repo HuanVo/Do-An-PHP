16acd41749c8c1a1205cf5175c84716cf05e74fd
Vo Van Huan <huanoaz@gmail.com>
2017-05-24 11:13:38 +0700

    Version one
    Phien Ban mot, chua co quan tri Admin.

---
+++ libs/Model.php
@@ -0,0 +1,69 @@
+<?php
+class Model{
+
+    public function __construct(){//kết nối
+		$this->conn=mysql_connect(__HOST,__USER,__PASS) or die ("Không thể kết nối server");
+		mysql_select_db(__DB_NAME,$this->conn) or die("Không thể kết nối CSDL");
+                mysql_query("SET NAMES 'utf8'"); 
+	}
+	public function disconnect(){//ngắt kết nối
+		if($this->conn){
+			@mysql_close($this->conn);
+		}
+	}
+        public function  exec($sql){// update, insert, delete
+            if (mysql_query($sql,$this->conn)) {
+                return  1;
+            } else {
+                    return 0;
+            }
+        }
+        // tra ve so dong
+        public function numrows($sql){
+            return mysql_num_rows(mysql_query($sql,$this->conn));
+        }
+        //truy van
+	public function query($sql){
+            $this->result=mysql_query($sql,$this->conn);
+	}
+	public function num_rows(){//đếm số dòng trả về từ câu lệnh truy vấn
+		if($this->result){
+		    $rows=mysql_num_rows($this->result);
+		}
+		else{
+		    $rows=0;
+		}
+		return $rows;
+	}
+	public function fetch()
+	{
+                if($this->result)
+		{
+                    if($this->num_rows()!=0){
+			while($row = mysql_fetch_array($this->result)){
+                            $this->data[]=$row;
+			}
+                    } else {
+                            $this->data=0;
+                        }
+                    
+		}
+		return $this->data;
+	 }
+	public function select($table, $where='')
+	{
+            if($where != ""){
+		if(is_array($where)){
+                    foreach($where as $k => $v){
+			$sql[]= "$k='$v'";
+                    }
+                    $where=implode(" and ",$sql);
+                    $where="where $where";
+                }else{
+                    $where="where $where";
+		}
+            }
+            $sql="select * from $table $where";
+            $this->query($sql);
+	}
+}
